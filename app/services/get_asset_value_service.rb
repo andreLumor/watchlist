da65032fee87ee9c8b3e1b5000b19ce8a2238b61
André Moreira <andre.moreira@codeminer42.com>
2022-04-05 18:17:59 -0300

    Create webcrawler tests

---
+++ app/services/get_asset_value_service.rb
@@ -0,0 +1,30 @@
+class GetAssetValueService
+  attr_reader :value
+  def initialize(asset, source_index)
+    @value = scrape_asset_value(asset, source_index)
+  end
+
+  private
+  ASSET_SOURCES = ['https://statusinvest.com.br/acoes/', 'https://www.infomoney.com.br/', 'https://investnews.com.br/cotacao/']
+
+  def scrape_asset_value(asset, source_index)
+    begin
+      doc = Nokogiri::HTML(URI.open(ASSET_SOURCES[source_index]+asset))
+    rescue
+      raise 'Access error'
+    else
+      return find_value_in_page(doc, source_index)
+    end
+  end
+
+  def find_value_in_page(doc, source_index)
+    case source_index
+    when 0
+      return doc.css('.special strong').text
+    when 1
+      return doc.css('.line-info .value p').text
+    when 2
+      return doc.css('.acao-valor').text.strip
+    end
+  end
+end
