8c5b9c14dcabc0cc3a382a7e9edfb7686db970dc
André Moreira <andre.moreira@codeminer42.com>
2022-04-07 15:18:31 -0300

    Fix job useless raise

--- app/sidekiq/quote_job.rb
+++ app/sidekiq/quote_job.rb
@@ -3,10 +3,7 @@ class QuoteJob
   sidekiq_options retry: 5
 
   def perform(ticker, source_index = rand(3))
-    if asset_value = GetAssetValueService.new(ticker, source_index).value
-      Asset.find_by(symbol: ticker).quotes.create!(price: asset_value)
-    else
-      raise 'Asset value could not be found'
-    end
+    asset_value = GetAssetValueService.new(ticker, source_index).value
+    Asset.find_by(symbol: ticker).quotes.create!(price: asset_value)
   end
 end
